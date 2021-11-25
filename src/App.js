3c2122ec9a96b90ee1a693bba13deb29fe603246
nviana <nvianaperez@gmail.com>
2021-11-25 12:22:39 +0100

    JS-poo-ES6 whit my comments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { UI } from "./UI.js";
 document
   .getElementById("product-form")
   .addEventListener("submit", function (e) {
-    // Override the default Form behaviour
+    // Override the default Form behaviour: 
+    // un formulario por defecto cuando se envía se refresca para esperar la respuesta del servidor. 
+    // le pasamos este evento a la función (e) y luego llamamos al evento con preventDefault() para cancelar este comportamiento por defecto 
     e.preventDefault();
 
     // Getting Form Values
@@ -15,19 +17,21 @@ document
 
     // Create a new Oject Product
     const product = new Product(name, price, year);
+    // const product = new Product (); se crea un objeto product con todas sus propiedades undefined
 
     // Create a new UI instance
-    const ui = new UI();
+    const ui = new UI(); //instancio un nuevo objeto ui de la clase UI
 
     // Input User Validation
     if (name === "" || price === "" || year === "") {
       ui.showMessage("Please Insert data in all fields", "danger");
+      return; //al llegar aquí, ya termina con la función y no sigue con el resto de código
     }
 
     // Save Product
-    ui.addProduct(product);
+    ui.addProduct(product); //y después de haber hecho la validación del input del usuario, accedo al método addProduct de la clase UI y le paso el objeto product para que lo añada
     ui.showMessage("Product Added Successfully", "success");
-    ui.resetForm();
+    ui.resetForm(); //llamo a resetear el formulario. clase.método objeto.método
   });
 
 document.getElementById("product-list").addEventListener("click", (e) => {
--- src/UI.js
+++ src/UI.js
@@ -2,8 +2,8 @@
 export class UI {
   // Add a new Product
   addProduct(product) {
-    const productList = document.getElementById("product-list");
-    const element = document.createElement("div");
+    const productList = document.getElementById("product-list"); //accedo a esta parte derecha del DOM
+    const element = document.createElement("div"); // con JS creo un elemento nuevo y lo lleno de HTML
     element.innerHTML = `
             <div class="card text-center mb-4">
                 <div class="card-body">
@@ -14,31 +14,35 @@ export class UI {
                 </div>
             </div>
         `;
-    productList.appendChild(element);
+    productList.appendChild(element); 
+    // una vez creado y rellenado, ahora hay que insertarlo 
+    // productList es el elemento padre, le insertamos un elemento hijo con appendChild(elemento hijo)
   }
 
+  // defino el método: una vez hago submit, el formulario se resetea para poder entrar otro producto
   resetForm() {
     document.getElementById("product-form").reset();
   }
 
+  // defino el método: deleteProduct, método de la clase UI
   deleteProduct(element) {
     if (element.name === "delete") {
       element.parentElement.parentElement.remove();
-      this.showMessage("Product Deleted Succsssfully", "success");
+      this.showMessage("Product Deleted Succsssfully", "success"); //this. proque quiero acceder a otra función que está dentro de esta misma clase
     }
   }
 
   showMessage(message, cssClass) {
     const div = document.createElement("div");
-    div.className = `alert alert-${cssClass} mt-2`;
-    div.appendChild(document.createTextNode(message));
+    div.className = `alert alert-${cssClass} mt-4`; // le doy una clase a este div que acabo de crear
+    div.appendChild(document.createTextNode(message)); // a div le añado un hijo que es el mensaje, y lo creo con createTextNode al momento
 
     // Show in The DOM
     const container = document.querySelector(".container");
     const app = document.querySelector("#App");
 
     // Insert Message in the UI
-    container.insertBefore(div, app);
+    container.insertBefore(div, app); //inserta el div en el container, antes de app
 
     // Remove the Message after 3 seconds
     setTimeout(function () {
